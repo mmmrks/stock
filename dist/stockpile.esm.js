79b1d91221f7f958b64131011da1eb44f35c9d40
markush <markus.holmroos@ambientia.fi>
2020-05-23 14:28:03 +0300

    ...

--- dist/stockpile.esm.js
+++ dist/stockpile.esm.js
@@ -4,15 +4,20 @@ var StockPile = function StockPile (data) {
   if ( data === void 0 ) data = {};
 
 
+  if( !data.pile ) { console.error("[StockPile]: You need to create at least pile object to your stash"); }
+
   this.stashVM = new Vue({ data: data });
 
 };
 
-var prototypeAccessors = { pile: { configurable: true } };
+var prototypeAccessors = { pile: { configurable: true },static: { configurable: true } };
 
 prototypeAccessors.pile.get = function () {
-  console.log(this.stashVM.$data.pile);
   return this.stashVM.$data.pile
+};
+
+prototypeAccessors.static.get = function () {
+  return this.stashVM.$data.static
 };
 
 Object.defineProperties( StockPile.prototype, prototypeAccessors );
@@ -20,44 +25,88 @@ Object.defineProperties( StockPile.prototype, prototypeAccessors );
 var stockpileplugin = {
   Stash: StockPile,
   install: function install (Vue, options) {
+
     Vue.mixin({
       data: function data() {
         var this$1 = this;
 
         return {
           stock: {
-            stash: {
-              pile: function (key,value) {
-
-                //console.log(this.$stock.stashVM)
-
-                var callerName = this$1.$options.name;
-
-                var schema = this$1.$stock.stashVM.$data.pile;
-                var pList = key.split('.');
-                var len = pList.length;
-                for(var i = 0; i < len-1; i++) {
-                    var elem = pList[i];
-                    if( !schema[elem] ) { schema[elem] = {}; }
-                    schema = schema[elem];
-                }
-
-
-                schema[pList[len-1]] = value;
-
-                if ( process.env.NODE_ENV === 'development' && typeof value === 'string' ) {
-                  console.log("%c" + "[STOCK PILE updated from "+callerName+"]: " + key + " Updated to value: " + value,  'color: #2673ff' );
-                } else { console.log("%c" + "[ STOCK updated from: "+callerName+" ] — Key:" + key + " Updated with object or array", 'color: #6fb7ff'); }
-
-              }
+            pile: {
+              stash: function (key,value) { return this$1.__stashParser(key,value,"pile",false); },
+              unstash: function (key,value) { return this$1.__stashParser(key,value,"pile",true); }
+            },
+            static: {
+              stash: function (key,value) { return this$1.__stashParser(key,value,"static"); }
             }
           }
         }
       },
       beforeCreate: function beforeCreate () {
         this.$stock = options;
+      },
+      methods: {
+
+        __stashParser: function __stashParser(key,value,type,unset) {
+
+          var callerName = this.$options.name || "[unnamed]";
+
+          var schema = this.$stock.stashVM.$data[type];
+          var pList = key.split('.');
+          var len = pList.length;
+          for(var i = 0; i < len-1; i++) {
+              var elem = pList[i];
+              if( !schema[elem] ) { schema[elem] = {}; }
+              schema = schema[elem];
+          }
+
+          if( type == "pile" )
+          {
+
+            if( unset ) {
+              schema[pList[len-1]] = null;
+            } else {
+              schema[pList[len-1]] = value;
+            }
+
+          }
+          else
+          {
+
+            if( !schema[pList[len-1]] )
+            {
+              schema[pList[len-1]] = value;
+            }
+            else
+            {
+              return console.error(("[StockPile Warning]: " + key + " already exist in Static stash"));
+            }
+
+          }
+
+          if( process.env.NODE_ENV != 'development' ) { return; }
+
+          if ( typeof value === 'string' )
+          {
+            console.log(("%c[StockPile]: " + (type.toUpperCase()) + " stash updated FROM " + callerName + " component — " + key + " = " + value), "color:#42b983");
+            if( callerName === "[unnamed]" ) { console.log("%c[StockPile WARNING]: It's recommended to name you components for better tracking", "color:#dccd53"); }
+          }
+          else if( typeof value === 'object' )
+          {
+            console.log(("%c[StockPile]: " + (type.toUpperCase()) + " stash updated FROM " + callerName + " component — " + key + " = " + (JSON.stringify(value))), "color:#42b983");
+            if( callerName === "[unnamed]" ) { console.log("%c[StockPile WARNING]: It's recommended to name you components for better tracking", "color:#dccd53"); }
+          }
+          else
+          {
+            console.log(("%c[StockPile]: " + (type.toUpperCase()) + " stash updated FROM " + callerName + " component — UNSTASHED the key " + key), "color:#42b983");
+            if( callerName === "[unnamed]" ) { console.log("%c[StockPile WARNING]: It's recommended to name you components for better tracking", "color:#dccd53"); }
+          }
+
+        }
+
       }
     });
+
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ class StockPile {
 
   constructor (data = {}) {
 
+    if( !data.pile ) console.error("[StockPile]: You need to create at least pile object to your stash");
+
     this.stashVM = new Vue({ data })
 
   }
 
   get pile () {
-    console.log(this.stashVM.$data.pile)
     return this.stashVM.$data.pile
+  }
+
+  get static () {
+    return this.stashVM.$data.static
   }
 
 }
@@ -18,42 +23,86 @@ class StockPile {
 const stockpileplugin = {
   Stash: StockPile,
   install (Vue, options) {
+
     Vue.mixin({
       data() {
         return {
           stock: {
-            stash: {
-              pile: (key,value) => {
-
-                //console.log(this.$stock.stashVM)
-
-                var callerName = this.$options.name;
-
-                var schema = this.$stock.stashVM.$data.pile
-                var pList = key.split('.');
-                var len = pList.length;
-                for(var i = 0; i < len-1; i++) {
-                    var elem = pList[i];
-                    if( !schema[elem] ) schema[elem] = {}
-                    schema = schema[elem];
-                }
-
-
-                schema[pList[len-1]] = value;
-
-                if ( process.env.NODE_ENV === 'development' && typeof value === 'string' ) {
-                  console.log("%c" + "[STOCK PILE updated from "+callerName+"]: " + key + " Updated to value: " + value,  'color: #2673ff' )
-                } else { console.log("%c" + "[ STOCK updated from: "+callerName+" ] — Key:" + key + " Updated with object or array", 'color: #6fb7ff') }
-
-              }
+            pile: {
+              stash: (key,value) => this.__stashParser(key,value,"pile",false),
+              unstash: (key,value) => this.__stashParser(key,value,"pile",true)
+            },
+            static: {
+              stash: (key,value) => this.__stashParser(key,value,"static")
             }
           }
         }
       },
       beforeCreate () {
         this.$stock = options
+      },
+      methods: {
+
+        __stashParser(key,value,type,unset) {
+
+          var callerName = this.$options.name || "[unnamed]";
+
+          var schema = this.$stock.stashVM.$data[type]
+          var pList = key.split('.');
+          var len = pList.length;
+          for(var i = 0; i < len-1; i++) {
+              var elem = pList[i];
+              if( !schema[elem] ) schema[elem] = {}
+              schema = schema[elem];
+          }
+
+          if( type == "pile" )
+          {
+
+            if( unset ) {
+              schema[pList[len-1]] = null;
+            } else {
+              schema[pList[len-1]] = value;
+            }
+
+          }
+          else
+          {
+
+            if( !schema[pList[len-1]] )
+            {
+              schema[pList[len-1]] = value;
+            }
+            else
+            {
+              return console.error(`[StockPile Warning]: ${key} already exist in Static stash`);
+            }
+
+          }
+
+          if( process.env.NODE_ENV != 'development' ) return;
+
+          if ( typeof value === 'string' )
+          {
+            console.log(`%c[StockPile]: ${type.toUpperCase()} stash updated FROM ${callerName} component — ${key} = ${value}`, "color:#42b983")
+            if( callerName === "[unnamed]" ) console.log(`%c[StockPile WARNING]: It's recommended to name you components for better tracking`, "color:#dccd53");
+          }
+          else if( typeof value === 'object' )
+          {
+            console.log(`%c[StockPile]: ${type.toUpperCase()} stash updated FROM ${callerName} component — ${key} = ${JSON.stringify(value)}`, "color:#42b983")
+            if( callerName === "[unnamed]" ) console.log(`%c[StockPile WARNING]: It's recommended to name you components for better tracking`, "color:#dccd53");
+          }
+          else
+          {
+            console.log(`%c[StockPile]: ${type.toUpperCase()} stash updated FROM ${callerName} component — UNSTASHED the key ${key}`, "color:#42b983")
+            if( callerName === "[unnamed]" ) console.log(`%c[StockPile WARNING]: It's recommended to name you components for better tracking`, "color:#dccd53");
+          }
+
+        }
+
       }
     })
+
   }
 }
 
